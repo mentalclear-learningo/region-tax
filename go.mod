fd43dff7553206722ed61d67fb4338bb1125e0c8
Dmitrii Kilishek <mentalclear@gmail.com>
2022-02-22 09:56:25 -0500

    Done here in this one

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/mentalclear-learingo/region-tax
 go 1.17
 
 require (
-	github.com/learning-go-book/simpletax v1.0.0
+	github.com/learning-go-book/simpletax/v2 v2.0.0
 	github.com/shopspring/decimal v1.3.1
 )
--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/learning-go-book/simpletax"
+	"github.com/learning-go-book/simpletax/v2"
 	"github.com/shopspring/decimal"
 )
 
 func main() {
-	amount, _ := decimal.NewFromString(os.Args[1])
-	zip := os.Args[2]
-	percent, err := simpletax.TaxForZip(zip)
+	amount, err := decimal.NewFromString(os.Args[1])
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
+	}
+	zip := os.Args[2]
+	country := os.Args[3]
+	percent, err := simpletax.ForCountryPostalCode(country, zip)
+	if err != nil {
+		log.Fatal(err)
 	}
 	total := amount.Add(amount.Mul(percent)).Round(2)
 	fmt.Println(total)
